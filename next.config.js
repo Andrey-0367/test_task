5e8680ad39a0d83e68b21a4bcf7b7e7eb34a682c
Andrey-0367 <shestakov0367@yandex.ru>
2025-04-22 08:30:48 +0300

    deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", 
+  generateBuildId: () => 'build-' + Date.now(),
+  onPostBuild: ({ outDir }) => {
+    require('fs').writeFileSync(outDir + '/.nojekyll', '')
+  },
   basePath: process.env.NODE_ENV === "production" ? "/test_task" : "", 
   assetPrefix: process.env.NODE_ENV === "production" ? "/test_task/" : "", 
   trailingSlash: true,
